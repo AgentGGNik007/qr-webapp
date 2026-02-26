515eef18d0d0ecf945e9dc81cceda034c4a134ab
Niklas <niklas.coding007@gmail.com>
2026-02-26 18:41:17 +0100

    Split config: move secrets to config.local.php

--- config/config.php
+++ config/config.php
@@ -1,16 +1,20 @@
 <?php
 declare(strict_types=1);
 
-return [
-    // SQLite-Datei für Benutzer
+$config = [
     'db_path'          => __DIR__ . '/../data/users.sqlite',
 
-    // Shlink-Anbindung
     'shlink_base_url'  => 'http://127.0.0.1:8081/rest/v3',
-    'shlink_api_key'   => 'culdria-change-me-please',
+    'shlink_api_key'   => null,
     'short_code'       => 'join',
 
-    // Allgemeines
     'base_url'         => 'https://culdria.framenode.net',
     'session_name'     => 'culdria_invite_session',
 ];
+
+if (file_exists(__DIR__ . '/config.local.php')) {
+    $local = require __DIR__ . '/config.local.php';
+    $config = array_merge($config, $local);
+}
+
+return $config;
